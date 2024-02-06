0184906d5c7f0632287b63b786642436ffbcf27d
lumengli7 <131196081+lumengli7@users.noreply.github.com>
2024-02-05 18:25:05 -0600

    static tree rendering

---
+++ Visualizer/src/components/pokedisplay.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Pokemon from "./pokemon.svelte";
+    export let pokes = []
+</script>
+
+{#each pokes as poke (poke.id)}
+    <Pokemon {poke}/>
+{/each}
---
+++ Visualizer/src/components/pokemon.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Card from "./card.svelte";
+    export let poke
+</script>
+
+<Card>
+    <div class="poke-name">
+        <p>Name: {poke.name}</p>
+        <p>Pokedex Number: {poke.id}</p>
+        <p>Type: {poke.type}</p>
+    </div>
+    <div class="close">X</div>
+</Card>
+
+<style>
+    .close {
+        position: absolute;
+        top: 10px;
+        right: 20px; 
+        cursor: pointer;
+    }
+</style>
