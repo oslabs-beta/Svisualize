6650652d1ff90ca5dac412fcc6a73ba741a2c1ed
Kris Fragata <79350471+krisfragata@users.noreply.github.com>
2024-02-06 19:33:14 -0500

    Delete Extension/svisualize directory

--- Extension/svisualize/src/webview.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from 'vscode';
-
-export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('extension.openWebView', () => {
-      // Create and show the WebView here
-  });
-
-  context.subscriptions.push(disposable);
-}
