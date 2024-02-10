b0ead6ce573632e0222880c783ca4d6f8686689a
lumengli7 <131196081+lumengli7@users.noreply.github.com>
2024-02-10 10:27:51 -0600

    finished parsing function
    Co-authored-by: Kris <kristinnefragata@outook.com>
    Co-authored-by: Caleb <calebbelkin@gmail.com>
    
    Co-authored-by: Jason <itsdayson@gmail.com>

--- svisualize/Jason-Caleb-parsing-test/demo-components2.0/child1.svelte
+++ svisualize/Jason-Caleb-parsing-test/demo-components2.0/child1.svelte
@@ -1,3 +1,4 @@
 <script>
   import grandchild1 from '/grandchild1.svelte';
+  import grandchild1B from '/grandchild1B.svelte';
 </script>
--- svisualize/dist/extension.js
+++ svisualize/dist/extension.js
@@ -266,7 +266,6 @@ const { traverseDirectory } = __webpack_require__(7);
 // push children to array and return result
 function parseFile(rootPath) {
     const filePaths = traverseDirectory(rootPath); //this will return an array of all file paths that end in .svelte
-    console.log(filePaths);
     let root; //raw code from App.svelte
     //parse through directories taken from traverDirectory function and find root
     filePaths.forEach((fileURI) => {
@@ -285,41 +284,49 @@ function parseFile(rootPath) {
         }
     }
     const componentStructure = new TreeNode('App');
-    console.log(componentStructure);
     let correctPath;
     let rootString = JSON.stringify(root);
+    function getFilePaths(rootPath) { }
     function parseFunc(fileContents = rootString, currTree = componentStructure) {
+        //only parse through text within script tags
+        let invoked = 0;
+        for (let i = 0; i < fileContents.length; i++) {
+            //if </script
+            if (fileContents[i] === '<') {
+                if (fileContents[i + 1] === '/') {
+                    if (fileContents[i + 2] === 's') {
+                        fileContents = fileContents.slice(0, i + 9);
+                        break;
+                    }
+                }
+            }
+            //splice after script
+        }
         //split file contents' into an array
         const fileContentsArr = fileContents
             .split(/[ ;'"]+/)
             .filter((word) => word.trim() !== '');
-        console.log(fileContentsArr);
         for (let i = 0; i < fileContentsArr.length; i++) {
+            //stop loop if file content text are outside of script tags
             if (fileContentsArr[i] === 'import') {
-                // children.push(fileContents[i + 1]);
                 //create a new instance of treeNode representing the new child
                 const newTreeNode = new TreeNode(fileContentsArr[i + 1]);
+                //declare a var to grab path [i + 3]
                 currTree.children.push(newTreeNode);
-                // for (let j = 0; j < filePaths.length; j++) {
-                //   if (filePaths[j].includes(fileContentsArr[i + 3])) {
-                //     const correctPath = filePaths[j];
-                //     fs.readFile(correctPath, 'utf-8', (err: string, source: string) => {
-                //       if (err) {
-                //         console.error('Error reading Svelte source file:', err);
-                //         return;
-                //       }
-                //       let newSourceString = JSON.stringify(source);
-                //       //children.push(fileContents[i + 1]);
-                //       //console.log(children);
-                //       // Consider if you really need recursion here
-                //       // return parseFunc(newSourceString);
-                //     });
-                //   }
-                // }
+                // console.log(newTreeNode);
+                for (let j = 0; j < filePaths.length; j++) {
+                    if (filePaths[j].includes(fileContentsArr[i + 3])) {
+                        const correctPath = filePaths[j];
+                        const childData = fs.readFileSync(correctPath, 'utf-8');
+                        let newSourceString = JSON.stringify(childData);
+                        parseFunc(newSourceString, newTreeNode);
+                    }
+                }
             }
         }
     }
     parseFunc();
+    console.log(componentStructure);
     return componentStructure;
 }
 exports.parseFile = parseFile;
