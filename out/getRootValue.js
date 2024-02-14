285956a15cb7af12574c44d79dec1c456e0dec45
lumengli7 <131196081+lumengli7@users.noreply.github.com>
2024-02-14 13:08:20 -0600

    Delete out directory

--- out/getRootValue.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.data = exports.getRootValue = void 0;
-function getRootValue(data) {
-    console.log('woohoo', data);
-    return data;
-}
-exports.getRootValue = getRootValue;
-//# sourceMappingURL=getRootValue.js.map
