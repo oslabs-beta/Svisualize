38e5174444315768b0ca721f92631093dd40b1d1
caleb Belkin <calebbelkin@gmail.com>
2024-02-17 07:23:34 -0800

    created mocha branch

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,9 +1,11 @@
 import * as assert from 'assert';
-
+require('dotenv').config();
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import { getSvelteFiles } from '../getSvelteFiles';
+import { getRootContent } from '../rootContent';
+import * as myExtension from '../extension';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
@@ -13,3 +15,24 @@ suite('Extension Test Suite', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
 	});
 });
+
+suite('Extension Suite', () => {
+	// const root = '';
+	let rootPath = '' ;
+
+	test('getSvelteFiles should return an array or string', () => {
+		const result = getSvelteFiles(rootPath);
+		if(rootPath === ''){
+			assert.equal(typeof result, 'string');
+		}else{
+			assert.equal(Array.isArray(result), true);
+		}
+	});
+
+	test('rootContent function should return a string', () => {
+		const results = getRootContent(rootPath);
+		assert.deepEqual(typeof results, 'string');
+	});
+
+});
+
