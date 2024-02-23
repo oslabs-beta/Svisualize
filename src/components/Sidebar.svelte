2786040f4d6905bfaf06422fa60ead2aa4f5bded
lumengli7 <131196081+lumengli7@users.noreply.github.com>
2024-02-23 16:03:35 -0600

    fixed some bugs

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -35,35 +35,11 @@
     
   </main>
 
-  <style>
-    h1 {
-      color: #EFD2A9;
-      text-align: center;
-      font-family: "Arial";      
-    }
-    
+  <style>  
     .header {
       display: flex;
       flex-direction: column;
       align-items: center;
       margin-top: 5px;
-    }
-
-    button {
-      width: 10em;
-      height: 2em;
-      background-color: #2a2a2a;
-      color: #e3ae52;
-      border-radius: 5px;
-      border: 1px solid #323232;
-      box-shadow: 2px 2px 3px #323232;
-      font-size: 16px;
-      font-weight: 600;
-      margin: 10px;
-    }
-
-    button:hover {
-      background-color: #dd9e46;
-      color: #2a2a2a;
     }
   </style>
